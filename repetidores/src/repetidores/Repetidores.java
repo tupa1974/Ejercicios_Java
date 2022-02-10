9f49d9576fe32b0c19a9b74b519714fa6436e387
tupa1974 <tupa1974@gmail.com>
2022-02-09 23:56:18 -0300

    agregado de ciclo for a ejemplos de repetidores

--- repetidores/src/repetidores/Repetidores.java
+++ repetidores/src/repetidores/Repetidores.java
@@ -1,16 +1,17 @@
-
 package repetidores;
 
 public class Repetidores {
 
-
     public static void main(String[] args) {
-        int cont =0;
-        while  (cont <=10){
-            cont ++;
-            System.out.println("Estoy en la vuelta "+ cont);
-            
+        int cont = 0;
+        while (cont <= 10) {
+            System.out.println("Estoy en la vuelta " + cont + " del while");
+            cont++;
+        }
+        for (int i = 0; i <= 10; i++ ){
+            System.out.println("Estoy en la vuelta " + i + " del for");
         }
     }
-    
+       
+
 }
