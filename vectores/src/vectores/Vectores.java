d3a82732e686bd6f0c1b9e5640c7c73be3edeecf
tupa1974 <tupa1974@gmail.com>
2022-02-10 00:33:35 -0300

    Ejercicio con vectores

---
+++ vectores/src/vectores/Vectores.java
@@ -0,0 +1,26 @@
+package vectores;
+
+import java.util.Scanner;
+
+public class Vectores {
+
+    public static void main(String[] args) {
+        // Declaracíon del vector
+
+        int vector[] = new int[5];
+
+        // Asignación del vector 
+        for (int indice = 0; indice < 5; indice++) {
+
+            System.out.println("Asigne el valor de la posición " + indice + " del vector: ");
+            Scanner valorTecleado = new Scanner(System.in);
+            vector[indice] = valorTecleado.nextInt();
+            System.out.println("Acabas de ingresar en la posición " + indice + " del vector el valor " + vector[indice]);
+        }
+        System.out.println("Los valores del arreglo son : ");
+        for (int recorrer = 0; recorrer < 5; recorrer++) {
+            System.out.println("Posición " + recorrer + " valor " + vector[recorrer]);
+        }
+    }
+
+}
